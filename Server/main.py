336c336ced639c5d67d38dd17de969ef435ddb5c
Yatarox <Faboto@yahoo.com>
2025-04-08 15:16:28 +0200

    ajout des contacts

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-from Server.auth import auth_bp
+from auth import auth_bp
 from config.config import Config
+
+from contact import contact_bp
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(auth_bp)
+app.register_blueprint(contact_bp)
+
 @app.route('/')
 def index():
     return render_template('index.html')
